dcfde74959ff4fa43ad85894a537f9490bb223fb
working-zima <working.zima@gmail.com>
2024-09-19 20:28:14 +0900

    feat: 에러 미들웨어에 토큰 만료 메시지 추가

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -1,6 +1,12 @@
 function errorMiddleware(error, req, res, next) {
+  if (error.name === 'TokenExpiredError') {
+    error.statusCode = 401;
+    error.message = 'TokenExpired';
+  }
+  console.log(`errorMiddleware`, error)
   // Internal Server Error 서버 내부 문제 발생 응답 코드
   const status = error.statusCode || 500;
+
   res.status(status).json({ message: error.message, data: error.data });
 }
 
