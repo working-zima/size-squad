533d041b98d4656b26182af3be81fb08271c6e95
working-zima <working.zima@gmail.com>
2024-09-27 00:20:49 +0900

    style: 서버 env 포트 이름 변경

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require("./src/db/index");
 /* 앱 실행 */
 const { app } = require("./src/app");
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 /* 포트 연결, 서버 실행 */
 app.listen(PORT, () => {
